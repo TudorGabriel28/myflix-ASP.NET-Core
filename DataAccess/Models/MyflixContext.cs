7912ebf2d4390ce19c9d15dc0b39983baa0f876e
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-03-03 15:35:46 +0200

    Create MyflixContext.

---
+++ DataAccess/Models/MyflixContext.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Models
+{
+	public partial class MyflixContext : DbContext
+	{
+		public MyflixContext() : base()
+		{
+		}
+
+		public MyflixContext(DbContextOptions<MyflixContext> options) : base(options)
+		{
+		}
+
+        public DbSet<User> Users { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			OnModelCreatingPartial(modelBuilder);
+		}
+		partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+	}
+}
