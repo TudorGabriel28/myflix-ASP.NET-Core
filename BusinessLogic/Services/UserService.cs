1c5770f81810d325fc22eee38287db3090731f81
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-03-03 15:34:31 +0200

    Create BusinessLogic project and add User Service.

---
+++ BusinessLogic/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLogic.Interfaces;
+using DataAccess.Models;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _repository;
+
+        public UserService(IRepository<User> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<User> Add(User user)
+        {
+            if (user == null)
+            {
+                return null;
+            }
+
+            return await _repository.AddAsync(user);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<User> GetById(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<User> Update(User user)
+        {
+            if (user == null)
+            {
+                return null;
+            }
+
+            return await _repository.UpdateAsync(user);
+        }
+    }
+}
