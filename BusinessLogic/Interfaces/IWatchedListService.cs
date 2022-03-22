e95bc5adc607ee0aaef70c65266c675070a5a9ca
Tudor Gabriel <tudordangabriel28@gmail.com>
2022-03-22 22:57:38 +0200

    Create WatchedList service and controller

---
+++ BusinessLogic/Interfaces/IWatchedListService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IWatchedListService
+    {
+        Task<IEnumerable<Movie>> Get(int AccountId);
+        Task<bool> Add(int AccountId, int MovieId);
+        Task<bool> Remove(int AccountId, int MovieId);
+    }
+}
---
+++ myflix ASP.NET Core/Controllers/WatchedListController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLogic.Interfaces;
+using DataAccess.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myflix_ASP.NET_Core.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WatchedListController : BaseController
+    {
+        private readonly IWatchedListService _service;
+        public WatchedListController(IWatchedListService service)
+        {
+            _service = service;
+        }
+        // GET: <WishListController>
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> Get()
+        {
+            var result = await _service.Get(Account.Id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        // PUT <WishListController>/5
+        [Authorize]
+        [HttpPut("add/{MovieId}")]
+        public async Task<ActionResult> AddMovie(int MovieId)
+        {
+            var result = await _service.Add(Account.Id, MovieId);
+            if (result)
+            {
+                return Ok();
+            }
+            return BadRequest(new { message = "Movie can't be added to watchedlist" });
+        }
+
+        // PUT <WishListController>/5
+        [Authorize]
+        [HttpPut("remove/{MovieId}")]
+        public async Task<ActionResult> RemoveMovie(int MovieId)
+        {
+            var result = await _service.Remove(Account.Id, MovieId);
+            if (result)
+            {
+                return Ok();
+            }
+            return BadRequest(new { message = "Movie can't be removed from watchedlist" });
+        }
+    }
+}
